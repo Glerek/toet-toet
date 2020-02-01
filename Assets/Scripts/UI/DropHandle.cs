9fbb720c78535d5d270c9b3f45b7504dad0314eb
chenxinjizhe <jzzle15001014621@gmail.com>
2020-02-01 19:58:26 +0900

    add DragHandle files

---
+++ Assets/Scripts/UI/DropHandle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropHandle : MonoBehaviour
+{
+	void OnDrop(PointerEventData eventData)
+	{
+		RectTransform invPanel = transform as RectTransform;
+
+		if (!RectTransformUtility.RectangleContainsScreenPoint(invPanel, Input.mousePosition))
+		{
+			Debug.Log("aa");
+		}
+	}
+}
