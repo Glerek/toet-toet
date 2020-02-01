889f434fcfd65f4ce8decac895fd48ba861c68a3
chenxinjizhe <jzzle15001014621@gmail.com>
2020-02-01 13:07:01 +0900

    scripts for car objects

---
+++ Assets/Scripts/UI/CarLight.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private int hp;
+    private enum status { normal, broken, lost };
+}
