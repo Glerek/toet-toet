413b9a6a4f46328f26ffba4344418b26f3af6830
Edouard Moinard <e.moinard@gmail.com>
2020-02-01 20:21:23 +0900

    Added spawn point + camera following car

--- Assets/Scripts/CarObjects/GameManager.cs
+++ Assets/Scripts/CarObjects/GameManager.cs
@@ -3,31 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class GameManager : Singleton<CarManager>, PlayerAction.IPlayerActions
+public class GameManager : Singleton<GameManager>, PlayerAction.IPlayerActions
 {
+	public GameObject _spawnTarget = null; 
     public Car carPrefab;
 
     PlayerAction.PlayerActions input;
 
     private Car car;
+	public Car Car { get { return car; } }
+
     bool pushedAccel = false;
 
     void Awake()
     {
         input = new PlayerAction.PlayerActions(new PlayerAction());
         input.SetCallbacks(this);
+
+        car = Instantiate(carPrefab, _spawnTarget.transform.position, Quaternion.identity);
     }
 
     void OnEnable() => input.Enable();
     void OnDestroy() => input.Disable();
 
     void OnDisable() => input.Disable();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        car = Instantiate(carPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-    }
 
     // Update is called once per frame
     void Update()
