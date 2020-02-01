d076af77e640afca3a578d4b560234b2df574bc8
Edouard Moinard <e.moinard@gmail.com>
2020-02-02 01:19:12 +0900

    Cleaning your room makes you feel nice

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameManager : Singleton<GameManager>, PlayerAction.IPlayerActions
+{
+	[SerializeField]
+	private GameObject _spawnTarget = null;
+
+	[SerializeField]
+    private Car _carTemplate = null;
+
+    private PlayerAction.PlayerActions _input;
+    private Car _car;
+    private bool _pushedAcceleration = false;
+
+	public Car Car
+	{
+		get { return _car; }
+	}
+
+    void Awake()
+    {
+        _input = new PlayerAction.PlayerActions(new PlayerAction());
+        _input.SetCallbacks(this);
+
+        _car = Instantiate(_carTemplate, _spawnTarget.transform.position, Quaternion.identity);
+    }
+
+    void OnEnable() => _input.Enable();
+    void OnDestroy() => _input.Disable();
+
+    void OnDisable() => _input.Disable();
+
+    // Update is called once per frame
+    void Update()
+    {
+		_car.HandleMovement(_pushedAcceleration);
+    }
+
+    public void OnAccel(InputAction.CallbackContext context)
+    {
+        _pushedAcceleration = context.ReadValue<float>() == 1.0f;
+    }
+}
--- Assets/Scripts/UI/DrivingUI.cs
+++ Assets/Scripts/UI/DrivingUI.cs
@@ -5,25 +5,17 @@ using UnityEngine;
 public class DrivingUI : MonoBehaviour
 {
 	public List<TrunkItem> _inventory = new List<TrunkItem>();
-	public List<SubsystemUI> _subsystems = new List<SubsystemUI>();
-	public HitchhikerUI _hitchhiker = null;
+	[SerializeField]
+	private SubsystemUI _wheelsSubsystem = null;
 
-	public void Initialize()
-	{
-		
-	}
+	[SerializeField]
+	private SubsystemUI _lightsSubsystem = null;
+
+	public HitchhikerUI _hitchhiker = null;
 
 	private void Update()
 	{
-		float averageDurability = 0f;
-		for (int i = 0; i < GameManager.Instance.Car.WheelObjects.Length; i++)
-		{
-			averageDurability += GameManager.Instance.Car.WheelObjects[i].GetComponent<Wheel>().Durability;
-			averageDurability /= (i + 1);
-		}
-
-		_subsystems[0]._subsystemHpGauge.Display(averageDurability / Pickable.MAX_DURABILITY);
-
-		_subsystems[1]._subsystemHpGauge.Display(GameManager.Instance.Car.LightObject.GetComponent<CarLight>().Durability / Pickable.MAX_DURABILITY);
+		_wheelsSubsystem._subsystemHpGauge.Display(GameManager.Instance.Car.WheelsDurability / Pickable.MAX_DURABILITY);
+		_lightsSubsystem._subsystemHpGauge.Display(GameManager.Instance.Car.LightsDurability / Pickable.MAX_DURABILITY);
 	}
 }
