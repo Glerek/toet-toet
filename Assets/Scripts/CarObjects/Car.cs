2d58f22fa2fb4157d3d31472e53e8c830f755d40
long-long-float <niinikazuki@yahoo.co.jp>
2020-02-02 13:36:49 +0900

    Fixed accel and brake

--- Assets/Scripts/CarObjects/Car.cs
+++ Assets/Scripts/CarObjects/Car.cs
@@ -5,177 +5,165 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-	[SerializeField]
+    [SerializeField]
     private float _torque = 10.0f;
 
-	[SerializeField]
-	private float _durabilityDecreasePerSecond = 2f;
+    [SerializeField]
+    private float _durabilityDecreasePerSecond = 2f;
 
-	[SerializeField]
-	private List<Wheel> _wheels = new List<Wheel>();
-	public float WheelsDurability
-	{
-		get
-		{
-			float totalDurability = 0f;
-			for (int i = 0; i < _wheels.Count; i++)
-			{
-				totalDurability += _wheels[i].Durability;
-			}
+    [SerializeField]
+    private List<Wheel> _wheels = new List<Wheel>();
+    public float WheelsDurability
+    {
+        get
+        {
+            float totalDurability = 0f;
+            for (int i = 0; i < _wheels.Count; i++)
+            {
+                totalDurability += _wheels[i].Durability;
+            }
 
-			return totalDurability / (float)_wheels.Count;
-		}
-	}
+            return totalDurability / (float)_wheels.Count;
+        }
+    }
 
-	[SerializeField]
-	private List<CarLight> _lights = new List<CarLight>();
-	public float LightsDurability
-	{
-		get
-		{
-			float totalDurability = 0f;
-			for (int i = 0; i < _lights.Count; i++)
-			{
-				totalDurability += _lights[i].Durability;
-			}
+    [SerializeField]
+    private List<CarLight> _lights = new List<CarLight>();
+    public float LightsDurability
+    {
+        get
+        {
+            float totalDurability = 0f;
+            for (int i = 0; i < _lights.Count; i++)
+            {
+                totalDurability += _lights[i].Durability;
+            }
 
-			return totalDurability / (float)_lights.Count;
-		}
-	}
+            return totalDurability / (float)_lights.Count;
+        }
+    }
 
-	private bool _duringAcceleration = false;
-	private bool _duringBreak = true;
-	private bool _canMove = true;
-	public bool CanMove
-	{
-		get { return _canMove; }
-		private set
-		{
-			if (_canMove != value)
-			{
-				_canMove = value;
+    private bool _duringAcceleration = false;
+    private bool _duringBreak = true;
+    private bool _canMove = true;
+    public bool CanMove
+    {
+        get { return _canMove; }
+        private set
+        {
+            if (_canMove != value)
+            {
+                _canMove = value;
 
-				DrivingUI.Instance.Display(_canMove);
-				RepairingUI.Instance.Display(!_canMove);
+                DrivingUI.Instance.Display(_canMove);
+                RepairingUI.Instance.Display(!_canMove);
 
-				foreach (var wheel in _wheels)
-				{
-					wheel.GetComponent<Rigidbody2D>().constraints = value ? RigidbodyConstraints2D.None : RigidbodyConstraints2D.FreezeAll;
-				}
-			}
-		}
-	}
+                foreach (var wheel in _wheels)
+                {
+                    wheel.GetComponent<Rigidbody2D>().constraints = value ? RigidbodyConstraints2D.None : RigidbodyConstraints2D.FreezeAll;
+                }
+            }
+        }
+    }
 
-	private Action _onVehicleStuck = null;
-	public event Action OnVehicleStuck
-	{
-		add 
-		{
-			_onVehicleStuck -= value;
-			_onVehicleStuck += value;
-		}
+    private Action _onVehicleStuck = null;
+    public event Action OnVehicleStuck
+    {
+        add
+        {
+            _onVehicleStuck -= value;
+            _onVehicleStuck += value;
+        }
 
-		remove
-		{
-			_onVehicleStuck -= value;
-		}
-	}
+        remove
+        {
+            _onVehicleStuck -= value;
+        }
+    }
 
     void Start()
     {
-		for (int i = 0; i < _wheels.Count; i++)
-		{
-			_wheels[i].OnBreakAction += OnWheelBroken;
-		}
+        for (int i = 0; i < _wheels.Count; i++)
+        {
+            _wheels[i].OnBreakAction += OnWheelBroken;
+        }
 
         StartCoroutine(DecreaseDurability());
     }
 
-	private void OnWheelBroken(Pickable wheel)
-	{
-		wheel.OnBreakAction -= OnWheelBroken;
+    private void OnWheelBroken(Pickable wheel)
+    {
+        wheel.OnBreakAction -= OnWheelBroken;
 
-		bool allWheelsBroken = true;
-		for (int i = 0; i < _wheels.Count; i++)
-		{
-			allWheelsBroken &= !_wheels[i].CanWork();
-		}
+        bool allWheelsBroken = true;
+        for (int i = 0; i < _wheels.Count; i++)
+        {
+            allWheelsBroken &= !_wheels[i].CanWork();
+        }
 
-		CanMove = !allWheelsBroken;
-	}
+        CanMove = !allWheelsBroken;
+    }
 
     private IEnumerator DecreaseDurability()
     {
-		float timer = 0f;
+        float timer = 0f;
         while (true)
         {
-			timer = 0f;
+            timer = 0f;
 
-			if (_duringAcceleration)
-			{
-				foreach (var wheel in _wheels)
-				{
-					wheel.DecreaseDurability(_durabilityDecreasePerSecond);
-				}
+            if (_duringAcceleration)
+            {
+                foreach (var wheel in _wheels)
+                {
+                    wheel.DecreaseDurability(_durabilityDecreasePerSecond);
+                }
 
-				foreach(var light in _lights)
-				{
-					light.DecreaseDurability(_durabilityDecreasePerSecond);
-				}
+                foreach (var light in _lights)
+                {
+                    light.DecreaseDurability(_durabilityDecreasePerSecond);
+                }
 
-				while (timer < 1f && _duringAcceleration)
-				{
-					timer += Time.deltaTime;
-					yield return null;
-				}
-			}
+                while (timer < 1f && _duringAcceleration)
+                {
+                    timer += Time.deltaTime;
+                    yield return null;
+                }
+            }
 
-			yield return null;
+            yield return null;
         }
     }
 
-    public void HandleMovement(bool accelerate, bool breaking)
+    public void Accel(bool enabled)
     {
-		_duringAcceleration = accelerate;
-		_duringBreak = breaking;
+        _duringAcceleration = enabled;
 
-		if (_duringAcceleration && _canMove)
-		{
-			_duringBreak = false;
-			foreach (var wheel in _wheels)
-			{
-				if (wheel.CanWork())
-				{
-					wheel.GetComponent<Rigidbody2D>().AddTorque(-_torque, ForceMode2D.Force);
-				}
-				else
-				{
-					_duringAcceleration = false;
-				}
-			}
-		}
-		else if(_duringBreak)
-		{
-			_duringAcceleration = false;
-			foreach (var wheel in _wheels)
-			{
-				if (wheel.CanWork())
-				{
-					if (_canMove)
-					{
-						wheel.GetComponent<Rigidbody2D>().AddTorque(+_torque, ForceMode2D.Force);
-					}
-				}
-				else
-				{
-					_duringBreak = false;
-				}
-			}
-		}
-		else
-		{
-			_duringAcceleration = false;
-			_duringBreak = false;
-		}
-	}
+        if (enabled && _canMove)
+        {
+            foreach (var wheel in _wheels)
+            {
+                if (wheel.CanWork())
+                {
+                    wheel.GetComponent<Rigidbody2D>().AddTorque(-_torque, ForceMode2D.Force);
+                }
+            }
+        }
+    }
+
+    public void Brake(bool enabled)
+    {
+        _duringBreak = enabled;
+
+        if (enabled && _canMove)
+        {
+            foreach (var wheel in _wheels)
+            {
+                if (wheel.CanWork())
+                {
+
+                    wheel.GetComponent<Rigidbody2D>().AddTorque(_torque, ForceMode2D.Force);
+                }
+            }
+        }
+    }
 }
