6684f6ee6f477b303a4ed6e7e62b66e1b4ad4f08
Edouard Moinard <e.moinard@gmail.com>
2020-02-02 15:08:20 +0900

    Added wheel structure

--- Assets/Scripts/CarObjects/Wheel.cs
+++ Assets/Scripts/CarObjects/Wheel.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Wheel : Pickable
 {
+	private bool _attachedToVehicle = true;
+
     protected override void OnBroken()
 	{
 		SpriteRenderer.color = Color.red;
@@ -12,6 +14,7 @@ public class Wheel : Pickable
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var mag = collision.relativeVelocity.magnitude;
+		// Debug.Log(mag);
 
 		if (mag > GameManager.Instance.CollisionMagnitudeThreshold)
 		{
