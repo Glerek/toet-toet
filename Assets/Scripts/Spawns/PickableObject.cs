d3d745471f30648cfc81246a71c17a3579059cfb
Edouard Moinard <e.moinard@gmail.com>
2020-02-10 17:11:12 +0900

    Added glow material + glow anim

--- Assets/Scripts/Spawns/PickableObject.cs
+++ Assets/Scripts/Spawns/PickableObject.cs
@@ -10,11 +10,25 @@ public class PickableObject : MonoBehaviour, IPointerClickHandler
 
     private SubsystemData _data = null;
     public SubsystemData Data { get { return _data; } }
+
+    [SerializeField]
+    private float _shineWidthMin = 0.05f;
     
+    [SerializeField]
+    private float _shineWidthMax = 0.1f;
+
+    [SerializeField]
+    private float _animationDuration = 2f;
+
+    private Material _material = null;
+    private float _timer = 0f;
+
+
     public void Initialize(SubsystemData data)
     {
         _data = data;
         _icon.sprite = _data.Icon;
+        _material = _icon.material;
     }
 
     public void OnPointerClick(PointerEventData pointerEventData)
@@ -35,6 +49,31 @@ public class PickableObject : MonoBehaviour, IPointerClickHandler
         if (string.Equals(collider.tag, "Car"))
         {
             SpawnManager.Instance.UnregisterPickable(this);
+        }
+    }
+
+    private void Update()
+    {
+        if (_material != null)
+        {
+            float t = _timer / _animationDuration;
+            _material.SetFloat("_ShineLocation", Mathf.Lerp(0, 1, t));
+
+            if (_timer < 0.5f)
+            {
+                _material.SetFloat("_ShineWidth", Mathf.Lerp(_shineWidthMin, _shineWidthMax, t * 2f));
+            }
+            else
+            {
+                _material.SetFloat("_ShineWidth", Mathf.Lerp(_shineWidthMax, _shineWidthMin, (t - 0.5f) * 2f));
+            }
+
+            _timer += Time.deltaTime;
+
+            if (_timer > _animationDuration)
+            {
+                _timer = 0f;
+            }
         }
     }
 }
