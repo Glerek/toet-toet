35f8cfcb9412c7e6b444dbb683b0e2b5e246f421
Edouard Moinard <e.moinard@gmail.com>
2020-02-18 18:25:58 +0900

    Added finish line + death walls

---
+++ Assets/Scripts/Spawns/FinishLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+	[SerializeField]
+	private bool _victory = false;
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (string.Equals(collider.tag, "Car"))
+		{
+			(GameManager.Instance.CurrentGameMode as DrivingMode).FinishDrivingMode(_victory);
+		}
+	}
+}
